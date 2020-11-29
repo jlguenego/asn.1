82f2cc869122bec3459f7de19be90d1ab515a6cf
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-11-29 13:33:03 +0100

    added test framework

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,7 @@
-console.log('Try npm run lint/fix!');
+import {parse} from 'path';
 
-const longString =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
+import {ASN1ParserOptions} from './interfaces';
 
-const trailing = 'Semicolon';
-
-const why = 'am I tabbed?';
-
-export function doSomeStuff(
-  withThis: string,
-  andThat: string,
-  andThose: string[]
-) {
-  //function on one line
-  if (!andThose.length) {
-    return false;
-  }
-  console.log(withThis);
-  console.log(andThat);
-  console.dir(andThose);
-  return;
+export function asn1Parse(options: Partial<ASN1ParserOptions> = {}) {
+  console.log('options: ', options);
 }
-// TODO: more examples
---
+++ test/foo-protocol.spec.ts
@@ -0,0 +1,10 @@
+import assert from 'assert';
+
+import {asn1Parse} from '../src';
+
+describe('Base64 Unit Test', () => {
+  it('should parse the foo protocol', () => {
+    asn1Parse();
+    assert.strictEqual(1, 1);
+  });
+});
