34c1d014254a9d8d45dbfd0cbfc006d80598e2be
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-12-05 14:23:41 +0100

    lint

--- src/codec/der/decoder/actions/SequenceAction.ts
+++ src/codec/der/decoder/actions/SequenceAction.ts
@@ -1,7 +1,6 @@
 import dbg from 'debug';
 import {Action} from '../../../../Action';
 import {ActionType} from '../../../../actions/ActionType';
-import {ObjectCtxt} from '../../../../interfaces/ObjectCtxt';
 import {State} from '../../../../interfaces/State';
 import {readLengthOctets} from '../../../ber/decoder/misc';
 
@@ -19,6 +18,15 @@ export class SequenceAction extends Action {
       state.root = state.context.current;
     } else {
       // sequence inside sequence
+      const newSequence = {};
+      const key = Object.keys(state.context.current).length;
+      state.context.current[key] = newSequence;
+      state.context = {
+        current: newSequence,
+        length: -1,
+        index: 0,
+        parent: state.context,
+      };
     }
 
     const length = readLengthOctets(state);
