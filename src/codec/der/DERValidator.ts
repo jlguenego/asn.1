2488afc0035bb0f82eb0c7682ef5e289f6036ce7
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-12-10 19:53:08 +0100

    test ok

--- src/codec/der/DERValidator.ts
+++ src/codec/der/DERValidator.ts
@@ -2,10 +2,14 @@ import {ASN1Assignment} from '../../asn1/ASN1Assignment';
 import {ASN1ComponentType} from '../../asn1/ASN1ComponentType';
 import {ASN1NamedType} from '../../asn1/ASN1NamedType';
 import {ASN1Sequence} from '../../asn1/ASN1Sequence';
+import {ASN1TaggedType} from '../../asn1/ASN1TaggedType';
 import {ASN1Message} from '../../interfaces/ASN1Message';
 
 export class DERValidator {
   validate(assignment: ASN1Assignment, input: ASN1Message) {
+    if (assignment.type instanceof ASN1TaggedType) {
+      this.validateTaggedType(assignment.type, input);
+    }
     if (assignment.type instanceof ASN1Sequence) {
       this.validateSequence(assignment.type, input);
     }
@@ -13,7 +17,25 @@ export class DERValidator {
     return;
   }
 
+  validateTaggedType(taggedType: ASN1TaggedType, input: ASN1Message) {
+    if (input.tagClass !== taggedType.tag.tagClass) {
+      throw new Error(
+        'taggedType tagClass differ: ${input.tagClass} vs ${taggedType.tag.tagClass} '
+      );
+    }
+    if (input.tagCode !== taggedType.tag.tagCode) {
+      throw new Error(
+        'taggedType tagCode differ: ${input.tagCode} vs ${taggedType.tag.tagCode} '
+      );
+    }
+    if (taggedType.type instanceof ASN1Sequence) {
+      this.validateSequence(taggedType.type, (input.value as ASN1Message[])[0]);
+    }
+  }
+
   validateSequence(sequence: ASN1Sequence, input: ASN1Message) {
+    console.log('input: ', input);
+    console.log('sequence: ', sequence);
     for (let i = 0; i < sequence.components.length; i++) {
       this.validateComponent(
         sequence.components[i],
