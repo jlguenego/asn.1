5e8559632fb532dcee603b5ae627103bd553c2d7
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-12-11 09:38:04 +0100

    remove register/actions/state

--- src/ASN1Parser.ts
+++ src/ASN1Parser.ts
@@ -1,14 +1,9 @@
 import dbg from 'debug';
 
-import {ActionFactory} from './actions/ActionFactory';
-import {ActionType} from './actions/ActionType';
 import {BERDecode} from './codec/ber/decoder/BERDecoder';
-import {DERRegister} from './encoding-rules/der/DERRegistration';
 import {EncodingRule} from './EncodingRule';
 import {ASN1ParserOptions} from './interfaces';
 import {ASN1Message} from './interfaces/ASN1Message';
-import {Props} from './interfaces/Props';
-import {State} from './interfaces/State';
 
 const debug = dbg('asn.1:parser');
 
@@ -21,30 +16,8 @@ export class ASN1Parser {
     this.options = {...this.options, ...opts};
   }
 
-  parseOld(input: ArrayBuffer): Props {
-    this.register();
-    const state: State = {
-      encodingRule: this.options.encodingRule,
-      dataview: new DataView(input),
-      size: input.byteLength,
-      index: 0,
-      nextAction: ActionType.INIT,
-      root: undefined,
-      context: undefined,
-    };
-    while (state.nextAction !== ActionType.NONE) {
-      debug('state: ', state);
-
-      const action = ActionFactory.get(state.nextAction);
-      action.transform(state);
-    }
-    if (!state.root) {
-      throw new Error('The parser did not return a valid state.root');
-    }
-    return state.root;
-  }
-
   parse(input: ArrayBuffer): ASN1Message {
+    debug('parse start');
     if (
       [EncodingRule.BER, EncodingRule.CER, EncodingRule.DER].includes(
         this.options.encodingRule
@@ -55,11 +28,5 @@ export class ASN1Parser {
     throw new Error(
       'Encoding rule not yet implemented: ' + this.options.encodingRule
     );
-  }
-
-  register() {
-    if (this.options.encodingRule === EncodingRule.DER) {
-      DERRegister();
-    }
   }
 }
--- src/Action.ts
+++
@@ -1,8 +0,0 @@
-import {ActionType} from './actions/ActionType';
-import {State} from './interfaces/State';
-
-export abstract class Action {
-  abstract type: ActionType;
-
-  abstract transform(state: State): void;
-}
