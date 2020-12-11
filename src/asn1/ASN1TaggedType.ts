d8dbddef3c789743f343a47acfc6d44cb683add2
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-12-11 17:50:52 +0100

    IMPLICIT

--- src/asn1/ASN1TaggedType.ts
+++ src/asn1/ASN1TaggedType.ts
@@ -2,5 +2,9 @@ import {ASN1Tag} from './ASN1Tag';
 import {ASN1Type} from './ASN1Type';
 
 export class ASN1TaggedType {
-  constructor(public tag: ASN1Tag, public type: ASN1Type) {}
+  constructor(
+    public tag: ASN1Tag,
+    public type: ASN1Type,
+    public implicit: boolean
+  ) {}
 }
