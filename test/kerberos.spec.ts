e5f87b79a3dba65993f5265c30125bf5cc62e314
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-12-13 20:02:53 +0100

    refactor files location

--- test/kerberos.spec.ts
+++ test/kerberos.spec.ts
@@ -4,8 +4,8 @@ import {resolve} from 'path';
 import {inspect} from 'util';
 import dbg from 'debug';
 
-import kerberosJson from './data/kerberos.json';
-import kerberosValidatedJson from './data/kerberos.validated.json';
+import kerberosJson from './data/kerberos/krb-ap-req.json';
+import kerberosValidatedJson from './data/kerberos/krb-ap-req.validated.json';
 import krbApRepJson from './data/kerberos/krb-ap-rep.json';
 import krbApRepValidatedJson from './data/kerberos/krb-ap-rep.validated.json';
 import {ASN1, ASN1Message, ASN1MsgUtils, EncodingRule, sanitize} from '../src';
@@ -15,14 +15,17 @@ const debug = dbg('asn.1:test');
 describe('Kerberos Protocol', () => {
   it('test hex base64 conv', () => {
     const base64Msg = readFileSync(
-      resolve(__dirname, 'data/kerberos.base64.der'),
+      resolve(__dirname, 'data/kerberos/krb-ap-req.base64.der'),
       {
         encoding: 'utf8',
       }
     );
-    const hexMsg = readFileSync(resolve(__dirname, 'data/kerberos.hex.der'), {
-      encoding: 'utf8',
-    });
+    const hexMsg = readFileSync(
+      resolve(__dirname, 'data/kerberos/krb-ap-req.hex.der'),
+      {
+        encoding: 'utf8',
+      }
+    );
 
     const buf = Buffer.from(sanitize(base64Msg), 'base64').toString('hex');
     assert.deepStrictEqual(buf, sanitize(hexMsg));
@@ -30,7 +33,7 @@ describe('Kerberos Protocol', () => {
 
   it('test kerberos AP-REQ msg', () => {
     const output = ASN1.parseFileMsg(
-      resolve(__dirname, 'data/kerberos.hex.der'),
+      resolve(__dirname, 'data/kerberos/krb-ap-req.hex.der'),
       {
         encodingRule: EncodingRule.DER,
         format: 'hex',
@@ -42,9 +45,12 @@ describe('Kerberos Protocol', () => {
   });
 
   it('test kerberos asn.1 file from the RFC4120', () => {
-    const definition = readFileSync(resolve(__dirname, 'data/kerberos.asn1'), {
-      encoding: 'utf8',
-    });
+    const definition = readFileSync(
+      resolve(__dirname, 'data/kerberos/kerberos.asn1'),
+      {
+        encoding: 'utf8',
+      }
+    );
     const module = ASN1.getModuleFromStr(definition);
     // console.log('module: ', inspect(module, false, null, true));
     const validated = module.validate(kerberosJson as ASN1Message, 'GSS-API');
@@ -80,9 +86,12 @@ describe('Kerberos Protocol', () => {
   });
 
   it('validate AP-REP', () => {
-    const definition = readFileSync(resolve(__dirname, 'data/kerberos.asn1'), {
-      encoding: 'utf8',
-    });
+    const definition = readFileSync(
+      resolve(__dirname, 'data/kerberos/kerberos.asn1'),
+      {
+        encoding: 'utf8',
+      }
+    );
     const module = ASN1.getModuleFromStr(definition);
     const validated = module.validate(krbApRepJson as ASN1Message, 'GSS-API');
     // console.log('validated: ', inspect(validated, false, null, true));
